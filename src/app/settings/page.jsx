4ae5b4d4f7efab2bc8a94c06079ea6aed3068326
Guru901 <padhhle@gmail.com>
2024-02-27 02:10:19 +0530

    MAJOR CHANGE added edit profile feature and improved performance

--- src/app/settings/page.jsx
+++ src/app/settings/page.jsx
@@ -1,9 +1,19 @@
+"use client";
+
 import React from "react";
 import Nav from "../../Components/Nav";
 import { FaBell, FaFeather, FaGears, FaUser } from "react-icons/fa6";
+import { RiLogoutBoxFill } from "react-icons/ri";
 import Link from "next/link";
+import axios from "axios";
+import { useRouter } from "next/navigation";
 
 const Settings = () => {
+  const router = useRouter();
+  const logOut = async () => {
+    await axios.post("/api/user/logout");
+    router.push("/login");
+  };
   return (
     <div>
       <Nav />
@@ -14,7 +24,12 @@ const Settings = () => {
               <div>
                 <FaUser />
               </div>
-              <div className="mt-1">Edit Profile</div>
+              <div>
+                <button className="mt-1 w-[80vw] flex justify-start">
+                  Edit Profile
+                </button>
+                <p className="text-xs">still can't change username</p>
+              </div>
             </div>
           </Link>
         </div>
@@ -24,7 +39,7 @@ const Settings = () => {
               <div>
                 <FaGears />
               </div>
-              <div>Account</div>
+              <button className="w-[80vw] flex justify-start">Account</button>
             </div>
           </Link>
         </div>
@@ -34,7 +49,11 @@ const Settings = () => {
               <div>
                 <FaBell />
               </div>
-              <div>Notifications</div>
+              <div>
+                <button className="w-[80vw] flex justify-start">
+                  Notifications
+                </button>
+              </div>
             </div>
           </Link>
         </div>
@@ -44,9 +63,29 @@ const Settings = () => {
               <div>
                 <FaFeather />
               </div>
-              <div>Want a feature</div>
+              <div>
+                <button className="w-[80vw] flex justify-start">
+                  Give Feedback
+                </button>
+                <p className="text-xs">
+                  Ni diya too bhoot le jaayenge uthha ke
+                </p>
+              </div>
             </div>
           </Link>
+        </div>
+        <div className="setting flex flex-row p-4 gap-8">
+          <div className="card-title flex justify-start items-center gap-4 cursor-pointer">
+            <div>
+              <RiLogoutBoxFill />
+            </div>
+            <div className="w-full">
+              <button className="w-[80vw] flex justify-start" onClick={logOut}>
+                Logout
+              </button>
+              <p className="text-xs">gaddari karbe</p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
--- src/app/settings/profile/page.jsx
+++ src/app/settings/profile/page.jsx
@@ -1,7 +1,187 @@
-import React from "react";
+"use client";
+
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Spinner from "@/Components/Spinner";
+import { CldUploadWidget } from "next-cloudinary";
+import { useRouter } from "next/navigation";
+import Nav from "@/Components/Nav";
 
 const EditProfile = () => {
-  return <div>EditProfile</div>;
+  const [user, setUser] = useState();
+  const [loading, setLoading] = useState(true);
+  const [avatar, setAvatar] = useState("");
+  const [form, setForm] = useState();
+  const [error, setError] = useState("");
+
+  const router = useRouter();
+
+  const pswdCheck = () => {
+    if (form.newPassword !== form.confPassword) {
+      setError("Passwords dont match");
+      if (form.newPassword.length < 5) {
+        setError("Password Should atleat be of 6 characters");
+        return false;
+      }
+    } else return true;
+  };
+
+  const handleChange = (e) => {
+    setForm({
+      ...form,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const getUser = async () => {
+    try {
+      setLoading(true);
+      const { data } = await axios.post("/api/user/me");
+      setUser(data);
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+      setLoading(false);
+    }
+  };
+
+  const handleAvatarChange = async () => {
+    try {
+      setLoading(true);
+      const { data } = await axios.post("/api/user/editAvatar", {
+        avatar,
+      });
+
+      if (data.success) {
+        router.push("/profile");
+        setLoading(false);
+      }
+    } catch (error) {
+      console.log(error);
+      setLoading(false);
+    }
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (pswdCheck()) {
+      try {
+        setLoading(true);
+        const { data } = await axios.post("/api/user/editProfile", {
+          avatar: avatar,
+          oldPass: form.currPassword,
+          newPass: form.newPassword,
+        });
+
+        if (data.success) {
+          router.push("/profile");
+          setLoading(false);
+        } else {
+          setError(data.msg);
+          setLoading(false);
+        }
+      } catch (error) {
+        console.log(error);
+        setLoading(false);
+      }
+    }
+  };
+
+  useEffect(() => {
+    getUser();
+  }, []);
+
+  if (loading) return <Spinner />;
+
+  return (
+    <div>
+      <Nav redirect="/settings" />
+      <div className="py-6">
+        <div className="max-w-screen flex items-center justify-center flex-col gap-2">
+          <div className="w-52 h-52 rounded-full overflow-hidden flex justify-center items-center">
+            <img
+              className="object-cover"
+              src={avatar ? avatar : user?.avatar}
+              alt={user?.username}
+            />
+          </div>
+          <div className="min-w-full flex justify-center flex-col items-center gap-2">
+            <div className="flex gap-12">
+              <CldUploadWidget
+                uploadPreset="cf72ckgk"
+                onSuccess={(results) => {
+                  setAvatar(results.info.secure_url);
+                }}
+              >
+                {({ open }) => {
+                  return (
+                    <button
+                      type="button"
+                      onClick={() => open()}
+                      className="underline"
+                    >
+                      Change avatar
+                    </button>
+                  );
+                }}
+              </CldUploadWidget>
+              <button className="btn" onClick={handleAvatarChange}>
+                Confirm Avatar
+              </button>
+            </div>
+            <div className="flex flex-col justify-center items-center gap-12">
+              <div className="flex flex-col gap-4 items-center justify-center">
+                <div className="flex gap-2">
+                  <h1 className="text-xl">Username</h1>
+                  <h1 className="font-bold text-xl">{`- ${user?.username}`}</h1>
+                </div>
+                <div className="flex gap-2">
+                  <h1 className="text-xl">Role</h1>
+                  <h1 className="font-bold text-xl">
+                    {`- ${
+                      user?.username === "Phoenix"
+                        ? "Chhotu"
+                        : user.isAdmin
+                        ? "Admin"
+                        : "User"
+                    }`}
+                  </h1>
+                </div>
+              </div>
+
+              <div className="flex flex-col items-center justify-center gap-3">
+                <h1>Change your password</h1>
+                <form className="flex flex-col gap-3" onSubmit={handleSubmit}>
+                  <input
+                    type="password"
+                    className="input input-bordered w-80"
+                    placeholder="Current Password"
+                    name="currPassword"
+                    onChange={handleChange}
+                  />
+                  <input
+                    type="password"
+                    className="input input-bordered w-80"
+                    placeholder="New Password"
+                    name="newPassword"
+                    onChange={handleChange}
+                  />
+                  <input
+                    type="password"
+                    className="input input-bordered w-80"
+                    placeholder="Confirm New Password"
+                    name="confPassword"
+                    onChange={handleChange}
+                  />
+                  <p className="text-sm">{error}</p>
+                  <input type="submit" className="btn" />
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default EditProfile;
