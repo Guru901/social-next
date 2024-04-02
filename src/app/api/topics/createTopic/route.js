96e1409f45cdb809593cc48b39818c8fbbf5f195
Guru901 <avinashkaru842@gmail.com>
2024-04-03 01:10:37 +0530

    fixed db connect bug in topics

--- src/app/api/topics/createTopic/route.js
+++ src/app/api/topics/createTopic/route.js
@@ -1,7 +1,10 @@
+import { connect } from "@/dbconfig/connect";
 import Topic from "@/models/topicModel";
 import { NextResponse } from "next/server";
 
 export async function POST(request) {
+  connect();
+
   const req = await request.json();
 
   const { createdBy, topic } = req;
--- src/app/api/topics/getTopics/route.js
+++ src/app/api/topics/getTopics/route.js
@@ -1,7 +1,10 @@
+import { connect } from "@/dbconfig/connect";
 import Topic from "@/models/topicModel";
 import { NextResponse } from "next/server";
 
 export async function POST() {
+  connect();
+
   const topics = await Topic.find({});
 
   const response = NextResponse.json(topics);
