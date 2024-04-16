85f4a8326be0894770e35ab1866edd0864c9bc50
Gurvinder Singh <133991448+Guru901@users.noreply.github.com>
2024-04-16 21:29:54 +0530

    db connect slow fixd

--- src/dbconfig/connect.js
+++ src/dbconfig/connect.js
@@ -2,10 +2,7 @@ import mongoose from "mongoose";
 
 export const connect = async () => {
   try {
-    let connection;
-    if (!connection) {
-      connection = await mongoose.connect(process.env.MONGO_URI);
-    }
+    const connection = await mongoose.connect(process.env.MONGO_URI);
   } catch (error) {
     console.error("Error connecting to MongoDB:", error);
   }
