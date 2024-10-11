79ff4a949ac9094b1384f61c051a687f9ce7d147
Guru901 <avinashkaur842@mgail.com>
2024-10-11 15:27:00 +0530

    added analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -24,6 +24,7 @@ export default function RootLayout({ children }) {
       </head>
       <body className={`${inter.className}`}>
         <ReactQueryProvider>{children}</ReactQueryProvider>
+        <Analytics />
       </body>
     </html>
   );
